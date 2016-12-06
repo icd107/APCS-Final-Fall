7dcd51b6fdc772c1acc0e2c6e88efb769a3e900d
Isabel Dominik <icd107@live.com>
2016-12-06 13:51:36 -0800

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,59 @@
+public class Person
+{
+    private String name;
+    private int age;
+    private boolean isChild;
+    private double income;
+    
+    Person()
+    {
+        this.name = "";
+        this.age = 0;
+        this.isChild = false;
+        this.income = 0.0;
+    }
+    Person(String name, int age, boolean isChild, double income)
+    {
+        this.name = name;
+        this.age = age;
+        this.isChild = isChild;
+        this.income = income;
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+    public int getAge()
+    {
+        return age;
+    }
+    public boolean isChild()
+    {
+        return isChild;
+    }
+    public double getIncome()
+    {
+        return income;
+    }
+    public void ageUp()
+    {
+        age++;
+        if(age >= 18)
+        {
+            isChild = false;
+        }
+        else
+        {
+            isChild = true;
+        }
+    }
+    public void setIncome(double income)
+    {
+        this.income = income;
+    }
+    public void promote()
+    {
+        income *= 1.2;
+    }
+}
