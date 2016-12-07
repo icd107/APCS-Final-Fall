19136ff908f91ddd0248573fbb4bdc321f6a0cfc
Isabel Dominik <icd107@live.com>
2016-12-07 12:17:34 -0800

    Added most of the remaining methods
    Not done with evictPets, go over project requirements to make sure I have everything I need

--- ResidentialProperty.java
+++ ResidentialProperty.java
@@ -36,4 +36,120 @@ public class ResidentialProperty
         rooms = new ArrayList<Room>();
         pets = new ArrayList<Pet>();
     }
+    
+    double getPrice()
+    {
+        return price;   
+    }
+    boolean forSale()
+    {
+        return forSale;   
+    }
+    double getRent()
+    {
+        return rent;   
+    }
+    int getSquareFeet()
+    {
+        return squareFeet;   
+    }
+    String getName()
+    {
+        return name;   
+    }
+    String getResidents()
+    {
+        String str = "";
+        for(Person e : residents)
+        {
+            str += e;
+            str += "\n";
+        }
+        return str;
+    }
+    String getPets()
+    {
+        String str = "";
+        for(Pet e : pets)
+        {
+            str += e;
+            str += "\n";
+        }
+        return str;
+    }
+    String getRooms()
+    {
+        String str = ""
+        for(Room e : rooms)
+        {
+            str += e;
+            str += "\n"
+        }
+        return str;
+    }
+    Room getRoom(int index)
+    {
+        return rooms.get(index);
+    }
+    Room getRoom(String name)
+    {
+        for(Room e : rooms)
+        {
+            if(e.getName().equals(name))
+            {
+                return e;   
+            }
+        }
+    }
+    boolean canPayRent()
+    {
+        double totalIncome = 0;
+        for(Person e : residents)
+        {
+            totalIncome += e.getIncome();
+            if(totalIncome >= rent)
+            {
+                return true;   
+            }
+        }
+        return false;
+    }
+    int getNumberOfResidents()
+    {
+        return residents.length;   
+    }
+    int getNumberOfPets()
+    {
+        return pets.size();   
+    }
+    void putForSale()
+    {
+        forSale = true;   
+    }
+    void takeOffMarket()
+    {
+        forSale = false;   
+    }
+    void setPetsAllowed(int pets)
+    {
+        maxPetsAllowed = pets;   
+    }
+    void addPets(Pet pet)
+    {
+        pets.add(pet);   
+    }
+    //not finished
+    void evictMeanPets()
+    {
+        String str = "";
+        for(int i = 0; i < pets.size(); i++)
+        {
+            if(!pets.get(i).isFriendly())
+            {
+                str += pets.get(i).getName() + " ";
+                pets.remove(i);
+                i--;
+            }
+        }
+    }
 }
